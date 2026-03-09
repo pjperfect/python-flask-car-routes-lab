7cf43001b3f3ccdda49db7123d85e79800b641f2
Philip Olembo <pjole.kenya@gmail.com>
2026-03-09 23:46:07 +0300

    Lab: Introduction to Flask - Car Routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,22 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+existing_models = ["Beedle", "Crossroads", "M2", "Panique"]
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Welcome to Flatiron Cars"
+
+
+@app.route("/<string:model>")
+def car(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
